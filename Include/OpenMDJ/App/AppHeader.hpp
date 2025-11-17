28994cf4c3a029182054c75127f8e379368e65a0
BC100Dev <bechris.100@gmail.com>
2025-11-17 11:35:15 +0000

    Prepare development information

---
+++ Include/OpenMDJ/App/AppHeader.hpp
@@ -0,0 +1,6 @@
+#ifndef OPENMDJ_APP_HEADER_HPP
+#define OPENMDJ_APP_HEADER_HPP
+
+// insert content here
+
+#endif //OPENMDJ_APP_HEADER_HPP
---
+++ Sources/Application/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char** argv, char** envMap) {
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Project development is coming soon to your desktops!" << std::endl;
+    return 0;
+}
